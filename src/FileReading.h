d5807b953284dfbec8f5ba83c982103ac2c7700b
Zé <Z@Z-Freixo>
2017-04-02 17:52:00 +0100

    I don't understand

--- src/FileReading.h
+++ src/FileReading.h
@@ -8,8 +8,8 @@ using namespace std;
 template <class T>
 class FileReading {
 public:
-	bool readNodesInfo(Graph<T> graph, string fileName);
-	bool readRoadsInfo(Graph<T> graph, string fileInfo, string fileGeometry);
+	static bool readNodesInfo(Graph<T> graph, string fileName);
+	static bool readRoadsInfo(Graph<T> graph, string fileInfo, string fileGeometry);
 };
 
 
