6d28cb14d2ce38bff335fb6d3c53bbe898f88b52
Zé <Z@Z-Freixo>
2017-04-08 16:39:06 +0100

    changed vertex icons

--- src/Emergencies.cpp
+++ src/Emergencies.cpp
@@ -6,38 +6,43 @@ vector<Vehicle> generateVehicles(const Graph<NodeInformation> & graph, GraphView
 	int indexVertex;
 
 	do{
-		indexVertex = rand() % graph.getNumVertex() + 1;
+		indexVertex = rand() % graph.getNumVertex();
 	} while(graph.getVertexSet()[indexVertex]->getInfo().getType() != "");
 
 	Vehicle vehicle1("Ambulance", graph.getVertexSet()[indexVertex]->getInfo(), false);
+	gv->setVertexIcon(graph.getVertexSet()[indexVertex]->getInfo().getId(), "ambulance.png");
 	vehicles.push_back(vehicle1);
 
 	do{
-		indexVertex = rand() % graph.getNumVertex() + 1;
+		indexVertex = rand() % graph.getNumVertex();
 	} while(graph.getVertexSet()[indexVertex]->getInfo().getType() != "");
 
 	Vehicle vehicle2("Van", graph.getVertexSet()[indexVertex]->getInfo(), false);
+	gv->setVertexIcon(graph.getVertexSet()[indexVertex]->getInfo().getId(), "van.png");
 	vehicles.push_back(vehicle2);
 
 	do{
-		indexVertex = rand() % graph.getNumVertex() + 1;
+		indexVertex = rand() % graph.getNumVertex();
 	} while(graph.getVertexSet()[indexVertex]->getInfo().getType() != "");
 
 	Vehicle vehicle3("Motorcycle", graph.getVertexSet()[indexVertex]->getInfo(), false);
+	gv->setVertexIcon(graph.getVertexSet()[indexVertex]->getInfo().getId(), "motorcycle.png");
 	vehicles.push_back(vehicle3);
 
 	do{
-		indexVertex = rand() % graph.getNumVertex() + 1;
+		indexVertex = rand() % graph.getNumVertex();
 	} while(graph.getVertexSet()[indexVertex]->getInfo().getType() != "");
 
 	Vehicle vehicle4("Policecar", graph.getVertexSet()[indexVertex]->getInfo(), false);
+	gv->setVertexIcon(graph.getVertexSet()[indexVertex]->getInfo().getId(), "police.png");
 	vehicles.push_back(vehicle4);
 
 	do{
-		indexVertex = rand() % graph.getNumVertex() + 1;
+		indexVertex = rand() % graph.getNumVertex();
 	} while(graph.getVertexSet()[indexVertex]->getInfo().getType() != "");
 
 	Vehicle vehicle5("Firetruck", graph.getVertexSet()[indexVertex]->getInfo(), false);
+	gv->setVertexIcon(graph.getVertexSet()[indexVertex]->getInfo().getId(), "firemen.png");
 	vehicles.push_back(vehicle5);
 
 	return vehicles;
@@ -46,7 +51,7 @@ vector<Vehicle> generateVehicles(const Graph<NodeInformation> & graph, GraphView
 void RandomEmergency(Graph<NodeInformation> & graph, GraphViewer *gv){
 	int indexVertex;
 	do{
-		indexVertex = rand() % graph.getNumVertex() + 1;
+		indexVertex = rand() % graph.getNumVertex();
 	} while(graph.getVertexSet()[indexVertex]->getInfo().getType() != "");
 	int emergencyType = rand() % 5 + 1;
 
